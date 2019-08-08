1b6207bb1fc8f53fc7e7ff14a879bac0582eff13
Chris <chrishuang@noodoe.com>
2019-08-08 14:27:11 +0800

    Update samples.

--- app/src/main/java/com/example/googlemap/MainActivity.kt
+++ app/src/main/java/com/example/googlemap/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.googlemap
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import com.example.googlemap.model.MyItem
-import com.example.googlemap.util.MyItemReader
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
-import org.json.JSONException
 
 class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
@@ -34,25 +38,33 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun startDemo() {
-
-        googleMap?.let { map ->
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(51.503186, -0.126446), 10f))
-            mClusterManager = ClusterManager(this, map)
-            map.setOnCameraIdleListener(mClusterManager)
-
-            try {
-                readItems()
-            } catch (e: Exception) {
-                Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show()
-            }
-        }
+    private fun createMarker(context: Context, point: LatLng, bedroomCount: Int): MarkerOptions {
+        val marker = MarkerOptions()
+        marker.position(point)
+//        val px = context.resources.getDimensionPixelSize(R.dimen.map_marker_diameter)
+//        val markerView = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+//            R.layout.map_circle_text,
+//            null
+//        )
+        val markerView = layoutInflater.inflate(R.layout.map_circle_text, null)
+        markerView.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        markerView.layout(0, 0, 100, 100)
+        markerView.buildDrawingCache()
+        val bedNumberTextView = markerView.findViewById(R.id.bed_num_text_view) as TextView
+        val mDotMarkerBitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(mDotMarkerBitmap)
+        bedNumberTextView.setText(bedroomCount)
+        markerView.draw(canvas)
+        marker.icon(BitmapDescriptorFactory.fromBitmap(mDotMarkerBitmap))
+        return marker
     }
 
-    @Throws(JSONException::class)
-    private fun readItems() {
-        val inputStream = resources.openRawResource(R.raw.radar_search)
-        val items = MyItemReader().read(inputStream)
-        mClusterManager?.addItems(items)
+    private fun startDemo() {
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(-33.8696, 151.2094), 10f))
+
+        googleMap?.addMarker(createMarker(this, LatLng(-33.8696, 151.2094), 5))
     }
 }
